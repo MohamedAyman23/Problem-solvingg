4057cbdaf07190eb0ee442afaa567955ad63d34c
Mohamed Ayman <109590449+MohamedAyman23@users.noreply.github.com>
2025-05-07 10:36:40 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    double n = 0, k =0, a =0;
-
-    scanf("%lf%lf%lf", &n, &k, &a);
-    //printf("%f %f %f\n", n, k, a);
-    double result = ((n * k)/a);// 35.5
-    long long MyNum = result;//35
-    double MyRes = (result - MyNum);//.5
-    //printf("%lld\n", MyNum);
-    if((MyRes) > 0)
+int main()
+{
+    int N =0;
+    int Res =0;
+    scanf("%i", &N);
+    //printf("%i\n", N);
+    Res = ((N % 10));
+    //printf("%i\n", Res);
+    N /= 10;
+    //printf("%i\n", N);
+    if(N == 0 || Res == 0)
     {
-        printf("double\n");
-    }
-    else if((MyNum >= -2147483648) && (MyNum <= 2147483647))
-    {
-        printf("int\n");
+        printf("YES\n");
     }
     else{
-        printf("long long\n");
+        if((N % Res) == 0 || (Res % N) == 0)
+        {
+            printf("YES\n");
+        }
+        else
+        {
+            printf("NO\n");
+        }
     }
 
-
-  return 0;
+    return 0;
 }
